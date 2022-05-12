c054429e9817aa40a271521b5109ccaf9b047671
Ummepinki <salmaumme888@gmail.com>
2022-05-12 15:13:24 +0600

    data insert

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rjdo3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db('perfumeItems').collection('items');
+
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+
+        app.get('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const items = await itemsCollection.findOne(query);
+            res.send(items);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Running project');
