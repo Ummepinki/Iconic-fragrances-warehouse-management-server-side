e6246f399b824208bdf72916f6f8f4bda057743b
Ummepinki <salmaumme888@gmail.com>
2022-05-18 13:07:26 +0600

    items update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -10,6 +11,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+
+    }
+    const token = authHeader.split('')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+    })
+
+    next();
+}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rjdo3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -18,10 +36,28 @@ async function run() {
     try {
         await client.connect();
         const itemsCollection = client.db('perfumeItems').collection('items');
+        const addCollection = client.db('perfumeItem').collection('add');
+
+        //auth
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '2d'
+            });
+            res.send({ accessToken });
+        })
 
         app.get('/items', async (req, res) => {
             const query = {};
             const cursor = itemsCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+
+        // add item api
+        app.get('/items/add', verifyJWT, async (req, res) => {
+            const query = {};
+            const cursor = addCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
         });
@@ -39,13 +75,26 @@ async function run() {
             res.send(result);
         });
 
+
+
         //Delete
         app.delete('/items/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await itemsCollection.deleteOne(query);
             res.send(result);
-        })
+        });
+
+        //update 
+
+        app.delete('/items/item', async (req, res) => {
+            const updatedItem = req.body;
+            const query = { _id: ObjectId(updatedItem._id) };
+            const result = await itemsCollection.deleteOne(query, updatedItem);
+            // const newItem = req.body;
+            // const result = await itemsCollection.insertOne(newItem);
+            res.send(result);
+        });
 
     }
     finally {
